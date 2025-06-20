7550c992fb9edb027815c50f851ca973fc966020
Rashid <ao411@canterbury.ac.uk>
2025-06-20 15:03:59 +0100

    Update main.py and dependencies; add research out and requirements.txt

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
+
+
 from tools import search_tool, wiki_tool, save_tool
 
 
